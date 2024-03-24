65ad9f8d1e72133e5e2ddb4479d987b7052f8a48
h-alice <halice@lazylabs.cc>
2024-03-25 02:38:55 +0800

    Redis support

--- src/main_server.go
+++ src/main_server.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"context"
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// PageContent struct for rendering.
+type PageContent struct {
+	Counter string
+}
+
+// Redis Connection
+var (
+	client        *redis.Client
+	page_template *template.Template
 )
 
 func clientHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,8 +32,25 @@ func clientHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	client = redis.NewClient(&redis.Options{
+		Addr:     "redis:6379", // Assuming Redis is running locally
+		Password: "",           // No password
+		DB:       0,            // Default DB
+	})
+
+	// Test the Redis connection.
+	_, err := client.Ping(context.Background()).Result()
+	if err != nil {
+		log.Fatal("[x] Error connecting to Redis:", err)
+	}
+
+	// Load template.
+	page_template = template.Must(template.ParseFiles("page_template.html"))
+
 	http.HandleFunc("/", clientHandler)
 	log.Println("[+] Collecting God Boris Power...")
-	log.Println("[+] Server Starting Up.")
+	log.Println("[.] Server starting up and listening on port 8080.")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+	log.Println("[.] Server Shutting Down.")
 }
